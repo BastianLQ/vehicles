2b31be090acc06f698e3af2df22555fd55143a2d
BastianLQ <bastylopez@gmail.com>
2024-05-24 17:52:44 -0400

    print: car_data

--- app.py
+++ app.py
@@ -21,3 +21,5 @@ if disp_button:
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncio de venta de coches')
     fig = px.scatter(car_data, x="odometer", y="price")
     st.plotly_chart(fig, use_container_width=True)
+
+print(car_data)
